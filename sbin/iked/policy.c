e88e0463a6983ea138e91689eae60083f0cf9b66
Markus Friedl <mfriedl@gmail.com>
2023-11-07 15:00:13 +0100

    iked: responder: always prefer group from KE if policy matches

--- sbin/iked/policy.c
+++ sbin/iked/policy.c
@@ -1133,8 +1133,7 @@ proposals_match(struct iked_proposal *local, struct iked_proposal *peer,
 				continue;
 			type = tpeer->xform_type;
 
-			if (rekey && nodh == 0 && dhgroup >= 0 &&
-			    protoid == IKEV2_SAPROTO_ESP &&
+			if (nodh == 0 && dhgroup >= 0 &&
 			    type == IKEV2_XFORMTYPE_DH) {
 				if (dhforced)
 					continue;
